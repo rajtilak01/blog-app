71a10f0c4e1fa4a615a4db5da8be2cd521e4a92e
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2024-11-05 00:03:58 +0530

    about page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,6 +3,7 @@ import { buttonVariants } from '@/components/ui/button';
 import Link from 'next/link';
 import fs from "fs";
 import matter from 'gray-matter';
+import Image from 'next/image';
 
 
 const dirContent = fs.readdirSync("content", "utf-8")
@@ -59,7 +60,9 @@ const Blog = () => {
         {blogs.map((blog, index) => (
           <div key={index} className="rounded-lg shadow-md overflow-hidden  dark:border-2">
             {/* Blog post image */}
-            <img src={blog.image} alt={blog.title} className="w-full h-64 object-cover" />
+            <Image src={blog.image} alt={blog.title} className="w-full h-64 object-cover" 
+            width={500}   // Set a suitable width for your image
+            height={500}/>
             
             {/* Blog post content */}
             <div className="p-4">
--- components/onthispage.tsx
+++ components/onthispage.tsx
@@ -1,6 +1,5 @@
 "use client"
 import React, { useEffect, useState } from 'react';
-import parse from 'html-react-parser';
 
 // Define the type for headings
 type Heading = {
