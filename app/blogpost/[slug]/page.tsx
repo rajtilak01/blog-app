2a265020f8f8ec59b757469e4cac83c6590d7a13
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2024-11-04 21:25:58 +0530

    added blog page

--- app/blogpost/[slug]/page.tsx
+++ app/blogpost/[slug]/page.tsx
@@ -8,7 +8,7 @@ import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
 import {unified} from 'unified'
 import rehypePrettyCode from "rehype-pretty-code";
-import { transformerCopyButton } from '@rehype-pretty/transformers'
+import { transformerCopyButton } from '@rehype-pretty/transformers';
 import OnThisPage from "@/components/onthispage"
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeSlug from 'rehype-slug'
